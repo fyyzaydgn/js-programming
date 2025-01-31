395f889365187a4c1259b54c3f4acc6161a9ae29
Feyyaz Aydogan <feyyaz.aydogan.nl@hotmail.com>
2025-02-01 02:28:21 +0300

    initial commit

---
+++ js-programming/day01/Variables.js
@@ -0,0 +1,37 @@
+let studentName = "James";
+let studentAge = "25";
+let isFullTime = false;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isFullTime);
+console.log(typeof isFullTime);
+console.log(typeof studentName);
+
+studentName = "Ross";
+console.log(studentName);
+console.log("----------------------------------------------------------------");
+
+if (true) {
+  var a = 100;
+}
+
+console.log(a);
+
+console.log("----------------------------------------------------------------");
+
+var x = 300;
+var x = 500;
+console.log(x);
+console.log("----------------------------------------------------------------");
+
+const PI = 3.14;
+console.log(PI);
+
+// pi = 5.25;
+// console.log(pi);
+
+console.log("----------------------------------------------------------------");
+
+const MAXIMUM_LIMIT = 100_000;
+console.log(MAXIMUM_LIMIT);
